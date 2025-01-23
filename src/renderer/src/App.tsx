f6bc44a9f37d35a892194425693857d146577609
Ornel ZAMO <prototyped26@gmail.com>
2025-01-23 11:44:43 +0100

    Project initialization

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1 className="text-red-400"> Hello, Welcome to SATURNE</h1>
+      <p>Projet de développement. </p>
+      <button className="btn btn-primary">Start</button>
+      <button className="btn btn-secondary">NEXT</button>
+    </div>
+  )
+}
+
+export default App
